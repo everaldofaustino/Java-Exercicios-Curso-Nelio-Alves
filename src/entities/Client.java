ba80fcfeee441a2a14bd6688005aa65b297aaef6
Everaldo Faustino <everaldo_faustino@yahoo.com.br>
2026-03-23 12:45:36 -0300

    Finalizado

--- src/entities/Client.java
+++ src/entities/Client.java
@@ -1,8 +1,11 @@
 package entities;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Client {
+
+    private static final SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
     private String name;
     private String email;
@@ -45,6 +48,6 @@ public class Client {
 
     @Override
     public String toString() {
-        return name + "(" + birthDate + ")" +" - " + email;
+        return name + "(" + sdf.format(birthDate) + ")" +" - " + email;
     }
 }
