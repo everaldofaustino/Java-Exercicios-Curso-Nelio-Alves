1463faec2716ed4ebaa06aa416b6fab09e7f62d4
Everaldo Faustino <everaldo_faustino@yahoo.com.br>
2026-05-13 17:19:22 -0300

    Seller entity class

--- modulo-21-jdbc/demo-dao-jdbc/src/application/Program.java
+++ modulo-21-jdbc/demo-dao-jdbc/src/application/Program.java
@@ -1,13 +1,26 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 public class Program {
 
     static void main(String[] args) {
 
+        Locale.setDefault(Locale.US);
+
+        DateTimeFormatter fmt= DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+
         Department obj = new Department(1,"Books");
-        System.out.println(obj);
+        Seller seller = new Seller(21,"Bob","bob@gmail.com", LocalDate.parse("11/02/1990", fmt), 3000.0, obj );
+
+        System.out.println(seller);
 
     }
 }
