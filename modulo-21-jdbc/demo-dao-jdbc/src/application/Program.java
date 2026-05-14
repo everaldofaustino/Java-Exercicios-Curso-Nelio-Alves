b8121adc08f54c7d5b0b4b21c5c82e70ef82a587
Everaldo Faustino <everaldo_faustino@yahoo.com.br>
2026-05-14 11:46:35 -0300

    módulo 21 findByDepartment implementation

--- modulo-21-jdbc/demo-dao-jdbc/src/application/Program.java
+++ modulo-21-jdbc/demo-dao-jdbc/src/application/Program.java
@@ -7,6 +7,7 @@ import model.entities.Seller;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.Locale;
 
 public class Program {
@@ -24,6 +25,16 @@ public class Program {
         Seller seller = sellerDao.findById(3);
 
         System.out.println(seller);
+
+
+        System.out.println("\n=== TEST 2: seller findByDepartment =====");
+
+        Department department = new Department(2,null);
+        List<Seller> list = sellerDao.findByDepartment(department);
+
+        for (Seller obj : list){
+            System.out.println(obj);
+        }
 
     }
 }
