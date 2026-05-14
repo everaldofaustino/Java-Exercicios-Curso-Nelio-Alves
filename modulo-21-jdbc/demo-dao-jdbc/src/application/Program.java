5d1acdd3394163810c8e6d137e25110248ede4da
Everaldo Faustino <everaldo_faustino@yahoo.com.br>
2026-05-14 19:36:39 -0300

    módulo 21 delete implementation

--- modulo-21-jdbc/demo-dao-jdbc/src/application/Program.java
+++ modulo-21-jdbc/demo-dao-jdbc/src/application/Program.java
@@ -10,12 +10,15 @@ import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
+import java.util.Scanner;
 
 public class Program {
 
     static void main(String[] args) {
 
         Locale.setDefault(Locale.US);
+
+        Scanner sc = new Scanner(System.in);
 
         DateTimeFormatter fmt= DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
@@ -60,7 +63,15 @@ public class Program {
         System.out.println("Update completed");
 
 
+        System.out.println("\n=== TEST 6: seller delete =====");
 
+        System.out.println("Enter id for delete test: ");
+
+        int id = sc.nextInt();
+        sellerDao.deleteById(id);
+        System.out.println("Delete completed");
+
+        sc.close();
 
     }
 }
