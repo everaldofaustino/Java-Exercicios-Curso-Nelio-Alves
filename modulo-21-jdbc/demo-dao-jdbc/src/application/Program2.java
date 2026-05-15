7380ee7f96dd21a236e86ab38190340065cfe952
Everaldo Faustino <everaldo_faustino@yahoo.com.br>
2026-05-15 10:45:57 -0300

    modulo 21 department insert implementation

---
+++ modulo-21-jdbc/demo-dao-jdbc/src/application/Program2.java
@@ -0,0 +1,70 @@
+package application;
+
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.dao.SellerDao;
+import model.entities.Department;
+import model.entities.Seller;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program2 {
+
+    static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+
+        Scanner sc = new Scanner(System.in);
+
+        DateTimeFormatter fmt= DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+
+      /*  System.out.println("=== TEST 1: Department findById ===");
+
+        Department department = departmentDao.findById(3);
+
+        System.out.println(department);
+
+
+
+        System.out.println("\n=== TEST 2: seller findAll =====");
+        list = sellerDao.findAll();
+
+
+        for (Seller obj : list){
+            System.out.println(obj);
+        }
+
+*/
+        System.out.println("\n=== TEST 3: department insert =====");
+
+        Department newDepartment = new Department(null,"Almoxarifado");
+        departmentDao.insert(newDepartment);
+        System.out.println("Inserted! New id = " + newDepartment.getId());
+/*
+        System.out.println("\n=== TEST 5: seller update =====");
+
+        seller = sellerDao.findById(1);
+
+        seller.setName("Martha");
+        sellerDao.update(seller);
+        System.out.println("Update completed");
+
+
+        System.out.println("\n=== TEST 6: seller delete =====");
+
+        System.out.println("Enter id for delete test: ");
+
+        int id = sc.nextInt();
+        sellerDao.deleteById(id);
+        System.out.println("Delete completed");
+*/
+        sc.close();
+
+    }
+}
